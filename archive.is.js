f5e3aa998d9c183e0deb4e55292e015e674f9b18
Austin Saylor <wasaylor@users.noreply.github.com>
2016-09-14 13:45:27 -0400

    Add page support

--- archive.is.js
+++ archive.is.js
@@ -1,11 +1,23 @@
-function archiveIsOnClick(info, tab) {
+function archiveIsOnClickLink(info, tab) {
 	if (!info["linkUrl"]) { return; }
 
 	open('https://archive.is/?run=1&url=' + encodeURIComponent(info["linkUrl"]));
 }
 
-var id = chrome.contextMenus.create({
+function archiveIsOnClickPage(info, tab) {
+	if (!info["pageUrl"]) { return; }
+
+	open('https://archive.is/?run=1&url=' + encodeURIComponent(info["pageUrl"]));
+}
+
+var id0 = chrome.contextMenus.create({
 	"title": "Archive.is this link",
 	"contexts": ["link"],
-	"onclick": archiveIsOnClick
+	"onclick": archiveIsOnClickLink
 });
+
+var id1 = chrome.contextMenus.create({
+	"title": "Archive.is this page",
+	"contexts": ["page"],
+	"onclick": archiveIsOnClickPage
+})
